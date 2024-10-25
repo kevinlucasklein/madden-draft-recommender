5d337e7353806525cf4fba2f17e259efe0f7f1ea
Kevin Klein <kevinlucasklein@gmail.com>
2024-10-24 20:28:28 -0600

    Setting up project

---
+++ frontend/src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import MaddenTable from './lib/MaddenTable.svelte';
+</script>
+
+<main>
+  <h1>Madden NFL Ratings</h1>
+  <MaddenTable />
+</main>
+
+<style>
+  main {
+    padding: 1em;
+    max-width: 100%;
+    margin: 0 auto;
+  }
+  h1 {
+    text-align: center;
+    color: #ff3e00;
+  }
+</style>
